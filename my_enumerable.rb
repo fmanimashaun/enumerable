339e93974be3fa7c5477f4633b497afb4217a031
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-11-02 16:47:07 +0000

    add any method to enumerables

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,7 +1,13 @@
 module MyEnumerable
-  ## the all method checks if a condition is met by all member or returns fals
+  ## the all method checks if a condition is met by all member or returns false
   def all?
     each { |item| return false unless yield(item) }
     true
+  end
+  ## the any method checks if the condition is met by any member and returns true
+
+  def any?
+    each { |item| return true if yield(item) }
+    false
   end
 end
