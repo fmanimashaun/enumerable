53982c971bad1f6aa871f2ed7b8694beab830a6d
fmanimashaun <fisayo.animashaun@outlook.com>
2023-11-02 20:43:54 +0100

    Fix linter error

--- my_list.rb
+++ my_list.rb
@@ -10,7 +10,7 @@ class MyList
   end
 end
 list = MyList.new(1, 2, 3, 4, 5)
-puts "list.all? { |item| item > 0 } returns #{list.all? { |item| item > 0 }}"
+puts "list.all? { |item| item > 0 } returns #{list.all?(&:positive?)}"
 puts "list.any? { |item| item > 2 } returns #{list.any? { |item| item > 2 }}"
 print(list.filter(&:even?))
 print(list.filter(&:odd?))
