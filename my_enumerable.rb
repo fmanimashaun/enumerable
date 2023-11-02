9c4015635dc249d5bbfcd38036002c99f6f08b53
Demes Abera Ameneshoa <128508830+demesameneshoa@users.noreply.github.com>
2023-11-02 16:42:15 +0000

    added the all? method

--- my_enumerable.rb
+++ my_enumerable.rb
@@ -1,1 +1,7 @@
-
+module MyEnumerable
+  ## the all method checks if a condition is met by all member or returns fals
+  def all?
+    each { |item| return false unless yield(item) }
+    true
+  end
+end
